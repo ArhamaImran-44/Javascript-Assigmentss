90ad1e941bb8babf7b8504a8997c99d1e78f06a5
ArhamaImran-44 <arhamai829@gmail.com>
2026-02-21 13:06:04 +0500

    assigment no.1

---
+++ app.js
@@ -0,0 +1,17 @@
+//   Question No. 1
+// alert(" Hello, Welcome to my website!");
+
+
+//   Question No. 2
+// alert("Error! Please enter a valid password")
+
+//   Question No. 3
+// alert("Welcome to JS Land... \n  Happy Coding !")
+
+//   Question No. 4
+// alert("Welcome to Js Land...")
+// alert("Happy Coding!")
+
+
+//   Question No. 4
+// console.log("Hello...I can run JS through my web browser's console")
