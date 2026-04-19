c215ec448d1480ef12f2c4e4965f5c5dced69b2d
ArhamaImran-44 <arhamai829@gmail.com>
2026-04-19 16:50:59 +0500

    done

---
+++ Assigment 17-20/app.js
@@ -0,0 +1,17 @@
+// for (let i=1 ; i<=5; i++){
+//       document.write("Arhama " )
+// }
+
+// for (let i=1 ; i<=10; i++){
+//     document.write(i "<br>")
+// }
+
+// for(let i=1 ; i<=5 ; i--){
+//     console.log("*")
+// }
+
+
+// for(let i=1 ; i>= ; i--){
+//     console.log("*")
+ 
+// }
